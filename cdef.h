37b520a2f68d1d9905d35336eb66d665f6e78393
ycsoft <xd.yang@yangmuyao.com>
2016-01-26 19:16:31 +0800

    常用宏

---
+++ cdef.h
@@ -0,0 +1,15 @@
+#ifndef CDEF_H
+#define CDEF_H
+
+
+#define     CPP_C_BEGIN   #ifdef __cplusplus \
+                           extern "C"{ \
+                          #endif
+
+#define     CPP_C_END     #ifdef __cplusplus \
+                            } \
+                          #endif
+
+
+
+#endif // CDEF_H
