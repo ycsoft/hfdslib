0230893e25c53e8f5310f068332e1cb2f289f97b
XD.Yang <xd.yang@yangmuyao.com>
2016-01-26 14:33:13 +0800

    main

---
+++ main.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <list>
+
+#include "c_rb_tree.h"
+#include "c_list.h"
+
+#include "ctest.h"
+
+
+int main(void)
+{
+    test_list();
+    return 0;
+}
+
