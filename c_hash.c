31d0ab8c91a1a4f2d0b020640909c9524df33408
ycsoft <xd.yang@yangmuyao.com>
2016-01-28 08:35:12 +0800

    修改数组长度

--- c_hash.c
+++ c_hash.c
@@ -8,17 +8,19 @@
 #include "c_hash.h"
 
 
+static uint32_t dict_hash_function_seed;
+
 hash_node *lookup(c_map *map, _key_type key)
 {
-    uint32_t  hashval = hash_value(key);
-
+    uint32_t  hashval = map->hash(key, strlen(key)) ;//hash_value(key);
     _bool_t   find = False;
-
     hash_node *node = map->_store[ hashval ],*parent = NULL;
 
-    if ( !node ->inUse )
+//    printf("Key=%s\tKey Hash:%d\n",key,hashval);
+    if ( ! node->_isUsing )
+    {
         return node;
-
+    }
     for ( ; node != NULL ; node = node ->next)
     {
         parent = node;
@@ -31,6 +33,9 @@ hash_node *lookup(c_map *map, _key_type key)
 
     if (!find )
     {
+
+//        printf("Repeat\n");
+//        system("pause");
         node = hash_node_new();
         parent->next = node;
     }
@@ -39,15 +44,75 @@ hash_node *lookup(c_map *map, _key_type key)
 
 uint32_t hash_value(const char *key)
 {
-    uint32_t hashval = 0;
+    uint32_t hashval = 5381;
 
     for ( hashval = 0; *key != '\0'; key++)
     {
-        hashval +=  *key + 131 * hashval;
+        hashval +=  *key + 31 * hashval;
     }
 
     return hashval % MaxNodeCount;
 }
+uint32_t genHashValue_HF(const void *key, int len)
+{
+    uint32_t    hashval = dict_hash_function_seed;
+    size_t      i       = 0;
+
+    char *ptr = key;
+
+    for ( i = 0 ; i < len; ++i)
+    {
+        if (*ptr == '\0')
+            break;
+        hashval = hashval  + 31 * hashval + *ptr++;
+    }
+
+    return hashval % MaxNodeCount;
+
+}
+
+uint32_t genHashValue_Redis(const void *key, int len)
+{
+    uint32_t seed = dict_hash_function_seed;
+    const uint32_t m = 0x5bd1e995;
+    const int r = 24;
+
+    /* Initialize the hash to a 'random' value */
+    uint32_t h = seed ^ len;
+
+    /* Mix 4 bytes at a time into the hash */
+    const unsigned char *data = (const unsigned char *)key;
+
+    while(len >= 4) {
+        uint32_t k = *(uint32_t*)data;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h *= m;
+        h ^= k;
+
+        data += 4;
+        len -= 4;
+    }
+
+    /* Handle the last few bytes of the input array  */
+    switch(len) {
+    case 3: h ^= data[2] << 16;
+    case 2: h ^= data[1] << 8;
+    case 1: h ^= data[0]; h *= m;
+    };
+
+    /* Do a few final mixes of the hash to ensure the last few
+     * bytes are well-incorporated. */
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return (unsigned int)h%MaxNodeCount;
+}
+
 hash_node   *hash_node_new()
 {
     hash_node *node = (hash_node*)malloc(sizeof(hash_node));
@@ -55,6 +120,7 @@ hash_node   *hash_node_new()
     memset(node,0,sizeof(hash_node));
     node->key = (_key_type)malloc(sizeof(char)*KEY_LEN);
     memset(node->key,0,KEY_LEN);
+    node->value  = INVALID_VALUE;
     return node;
 }
 
@@ -73,6 +139,8 @@ c_map           *map_create(void)
     c_map *mp = (c_map*)malloc(sizeof(c_map));
     size_t i = 0;
 
+    dict_hash_function_seed = rand()% 13131;
+
     mp->_size = 0;
     for ( i = 0 ; i < MaxNodeCount; ++i)
     {
@@ -80,6 +148,7 @@ c_map           *map_create(void)
         mp->_store[i] = node;
     }
 
+    mp->hash = genHashValue_HF;
     mp->size = map_size;
     mp->get = map_get;
     mp->set = map_set;
@@ -97,7 +166,7 @@ _value_type     map_get(c_map *mp,_key_type key)
 {
     hash_node *node = lookup(mp,key);
 
-    if ( node->inUse )
+    if ( node->_isUsing )
     {
         return node->value;
     }
@@ -111,9 +180,8 @@ void            map_set(c_map *mp,_key_type key, _value_type value)
     save = lookup(mp,key);
 
     save->value = value;
+    save->_isUsing = True;
     _key_copy(save->key,key);
-    save->inUse = True;
-
 }
 
 void            map_free(c_map *mp)
@@ -139,9 +207,9 @@ void            map_erase(c_map *mp, _key_type key)
 {
     hash_node *node = lookup(mp,key);
 
-    if ( node->inUse )
+    if ( node->_isUsing )
     {
-       node->inUse = False;
+       node->_isUsing = False;
        memset(node->key,0,KEY_LEN);
     }
 
--- c_hash.h
+++ c_hash.h
@@ -29,15 +29,17 @@ struct hash_node
 {
     _key_type    key;
     _value_type  value;
-    _bool_t      inUse;
+    _bool_t      _isUsing;
     hash_node    *next;
 };
 
 struct c_map
 {
     uint32_t        _size;
-
     hash_node       *_store[MaxNodeCount];
+
+    hash_node       (*find_node)(c_map* mp);
+    uint32_t        (*hash)(const void* key, int len);
     uint32_t         (*size)(c_map* mp);
     _value_type     (*get)(c_map* mp ,_key_type key);
     void            (*set)(c_map* mp , _key_type key, _value_type value);
@@ -50,6 +52,8 @@ hash_node       *hash_node_new();
 
 /* caculate hash value of key*/
 uint32_t        hash_value( const char * key);
+uint32_t        genHashValue_Redis(const void *key, int len);
+uint32_t        genHashValue_HF(const void *key, int len);
 
 c_map           *map_create(void);
 uint32_t        map_size(c_map *mp);
