342b9e1ea0b1aae4252c2625c9e4864d43cfa356
ycsoft <xd.yang@yangmuyao.com>
2016-02-01 08:14:29 +0800

    动态类型支持

--- c_list.c
+++ c_list.c
@@ -7,25 +7,26 @@
 #include "c_list.h"
 #include "string.h"
 
-c_list_node * list_node_new()
+c_list_node * list_node_new( int unit)
 {
     c_list_node *nd = (c_list_node*)malloc(sizeof(c_list_node));
 
+    assert(nd->data!=NULL);
     if ( nd )
     {
         memset(nd,0,sizeof(c_list_node));
     }
-
+    nd->data = malloc(unit);
     return nd;
 }
 
-c_list * list_init()
+c_list * list_init( int unit)
 {
     c_list *list = (c_list*)malloc(sizeof(c_list));
 
-    c_list_node *head = list_node_new(),
-                *tail = list_node_new();
-
+    c_list_node *head = list_node_new(unit),
+                *tail = list_node_new(unit);
+    int i = 0;
 
     assert(list != NULL );
     assert(head != NULL );
@@ -39,25 +40,33 @@ c_list * list_init()
     list->head->next = list->tail;
     list->tail ->pre = list->head;
 
+    list->size = 0;
+
     return list;
 }
 void list_free(c_list *list)
 {
-    c_list_node *nd = list->head,*next;
+    uint32_t    i = 0;
+
+    c_list_node *node = NULL,*tmp;
 
     assert(list != NULL );
 
-    while(nd)
+    node = list->head;
+
+    while ( node )
     {
-        next = nd->next;
-        free(nd);
-        nd = next;
+        free(node->data);
+        tmp = node->next;
+        free(node);
+        node = tmp;
     }
+
     free(list);
 
 }
 
-void list_push_back(c_list *list, c_list_node *nd)
+void _list_push_back(c_list *list, c_list_node *nd)
 {
 
     assert(nd != NULL);
@@ -70,7 +79,22 @@ void list_push_back(c_list *list, c_list_node *nd)
     list->size++;
 }
 
-void list_push_front(c_list *list, c_list_node *nd)
+void list_push_back(c_list *list, void *value, int unit)
+{
+    uint32_t pos = list->size;
+    c_list_node *node = list_node_new(unit);
+    memcpy(node->data,value,unit);
+    _list_push_back(list,node);
+
+}
+void list_push_front(c_list *list, void *value,int unit)
+{
+    c_list_node *nd = list_node_new(unit);
+    memcpy(nd->data,value,unit);
+    _list_push_back(list,nd);
+}
+
+void _list_push_front(c_list *list, c_list_node *nd)
 {
 
    assert(list != NULL);
@@ -82,27 +106,34 @@ void list_push_front(c_list *list, c_list_node *nd)
    nd->pre = list->head;
 }
 
-c_list_node *list_read_back(c_list *list)
+c_list_node *_list_read_back(c_list *list)
 {
     assert(list);
     return list->tail->pre;
 }
 
-c_list_node *list_read_front(c_list *list)
+c_list_node *_list_read_front(c_list *list)
 {
     assert(list);
     return list->head->next;
 }
 
+void * list_read_back(c_list *list)
+{
+    return (_list_read_back(list)->data);
+}
+
+void *list_read_front(c_list *list)
+{
+    return (_list_read_front(list)->data);
+}
+
 void list_erase_back(c_list *list)
 {
-    c_list_node *nd = list->tail->pre;
+    c_list_node *nd = (list->tail->pre);
 
-    list->tail ->pre = nd->pre;
-    nd->pre ->next = list->tail;
-
-    free(nd);
-    nd = NULL;
+    list->tail ->pre = (nd)->pre;
+    (nd)->pre ->next = list->tail;
 }
 void list_erase_front(c_list *list)
 {
@@ -123,7 +154,7 @@ void list_erase(c_list *list, c_list_node *nd)
 
     while ( cur )
     {
-        if ( cur == nd )
+        if ( cur->data == nd->data )
         {
             cur->pre->next = cur->next;
             cur->next->pre = cur->pre;
--- c_list.h
+++ c_list.h
@@ -1,42 +1,115 @@
 #ifndef C_LIST_H
 #define C_LIST_H
 
+#define     LIST_INIT_SIZE      1000
+#define     LIST_INC_SIZE       1000
+
 typedef struct c_list_node c_list_node;
 typedef struct c_list c_list;
+
+
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+#define     HF_List     c_list
+
+#define     List_Create( Type ) \
+            list_init(sizeof(Type))
+
+#define     List_Free(list) \
+            list_free(list)
+
+#define     List_Push_Back(Type,list,v) \
+           {\
+                c_list_node *node = list_node_new(sizeof(Type)); \
+                *(Type*)(node->data) = v; \
+                _list_push_back(list,node);\
+           }
+
+#define     List_Push_Front(Type,list,v) \
+           {\
+                c_list_node *node = list_node_new(sizeof(Type)); \
+                *(Type*)node->data = v; \
+                _list_push_front(list,node);\
+           }
+
+#define     List_Erase_Back(list) \
+            list_erase_back(list);
+
+#define     List_Erase_Front(list) \
+            list_erase_front(list);
+
+#define     List_Erase(Type,list,v) \
+{\
+    c_list_node *cur = list->head->next;\
+    assert( nd != list->head && nd != list->tail );\         \
+    while ( cur )\
+    {\
+        if ( *(Type*)cur->data == *(Type*)nd->data )\
+        {\
+            cur->pre->next = cur->next;\
+            cur->next->pre = cur->pre;\
+            free(cur);\
+            break;\
+        }\
+        cur = cur->next;\
+        \
+    }\
+}
+
+#define     List_Free(list) \
+            list_free(list)
+
+#define     List_Read_Front(Type,list) \
+            *(Type*)(_list_read_front(list)->data)
+#define     List_Read_Back(Type,list) \
+            *(Type*)(_list_read_back(list)->data)
+
+#define     List_Begin(list)  list->head->next
+#define     List_End(list)    list->tail
+
 struct c_list_node
 {
     c_list_node *pre;
     c_list_node *next;
-    void   *data;
+    void        *data;
 };
 
 struct c_list
 {
     c_list_node     *head;
     c_list_node     *tail;
+    c_list_node     *iterator;
+
     uint32_t         size;
+
 };
 
-c_list_node*    list_node_new();
+c_list_node*    list_node_new(int unit);
 
-c_list*         list_init();
+c_list*         list_init(int unit);
 void            list_free(c_list *list);
 
-void    list_push_back(c_list *list, c_list_node *nd);
-void    list_push_front(c_list *list, c_list_node *nd);
+void    _list_push_back(c_list *list, c_list_node *nd);
+void    _list_push_front(c_list *list, c_list_node *nd);
+
+void    list_push_back (c_list *list, void *value, int unit);
+void    list_push_front(c_list *list, void *value, int unit);
 
 void    list_erase_back(c_list *list);
 void    list_erase_front(c_list *list);
 void    list_erase(c_list *list, c_list_node *nd);
 
-c_list_node *list_read_back(c_list *list);
-c_list_node *list_read_front(c_list *list);
+
+
+c_list_node *_list_read_back(c_list *list);
+c_list_node *_list_read_front(c_list *list);
+
+void *  list_read_back(c_list *list);
+void *  list_read_front(c_list *list);
 
 
 #ifdef __cplusplus
