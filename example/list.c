5f138a737ac2b8f764ed599dede2f15b158dc7cc
ycsoft <xd.yang@yangmuyao.com>
2016-02-01 09:09:34 +0800

    examples

--- example/list.c
+++ example/list.c
@@ -4,39 +4,32 @@
 #include <stdio.h>
 #include <assert.h>
 
-#include "c_vector.h"
+#include "c_list.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-typedef struct _point
+
+void  ex_list(void)
 {
-    int x;
-    int y;
-    int z;
-}Point;
 
-void  ex_vector(void)
-{
-    hfvector *vec = (phfvector)malloc(sizeof(hfvector));
-    Point p,p1,p2;
+    HF_List *list = List_Create(int);
 
-    Vector_Create(Point,vec,10);
-    p.x = 1;
-    p.y = 1;
-    p.z = 1;
-    Vector_Push_back(Point,vec,p);
-    p.x = 2;
-    p.y = 2;
-    p.z = 2;
-    Vector_Push_back(Point,vec,p);
+    int v;
 
-    p1 = Vector_Get(Point,vec,0);
-    p2 = Vector_Get(Point,vec,1);
 
-    Vector_Free(vec);
+    List_Push_Back(int,list,1);
+    List_Push_Back(int,list,2);
+    List_Push_Back(int,list,3);
+    list->iterator = List_Begin(list);
+    while ( list->iterator != List_End(list))
+    {
+        printf("%d\n",*(int*)list->iterator->data);
+        list->iterator = list->iterator->next;
+    }
+
 }
 #ifdef __cplusplus
 }
