e586aa1eab1121ccacd2cbe996d52331bed1a8f2
ycsoft <xd.yang@yangmuyao.com>
2016-02-24 10:42:53 +0800

    example show how to use dict

--- example/dict.c
+++ example/dict.c
@@ -1,34 +1,65 @@
-#include <stdlib.h>
+﻿#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
 
+#include "c_hash.h"
 #include "c_list.h"
+#include "c_rb_tree.h"
+#include "c_dict.h"
+#include "cdef.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+typedef struct   _VAL
+{
+    int a;
+    int b;
+}_VAL;
 
-void  ex_list(void)
+
+
+
+int char_cmp(void *k1, void *k2)
+{
+    printf("CMP====%s --- %s, return:%d\n",(char*)k1,(char*)k2,strcmp((char*)k1,(char*)k2));
+    return strcmp((char*)k1,(char*)k2);
+}
+
+int key_compare(void * left, void * right)
 {
 
-    HF_List *list = List_Create(int);
+}
 
-    int v;
+void  ex_dict(void)
+{
+    XD_String       sk1 = String("Yang"),//{{STRING,5,Default_Compare,Default_Copy},"Yang"},
+                    sv1 = String("Yang1") ,//{{STRING,5,Default_Compare,Default_Copy},"Yang1"},
+                    sk2 = String("Chen"),//{{STRING,5,Default_Compare,Default_Copy},"Chen"},
+                    sv2 = String("Chen2");//{{STRING,5,Default_Compare,Default_Copy},"Chen2"};
 
+    XD_INT          ik1 = Int(4),//{{INT,4,Default_Compare,Default_Copy},1},
+                    ik2 = Int(4);//{{INT,4,Default_Compare,Default_Copy},2};
 
-    List_Push_Back(int,list,1);
-    List_Push_Back(int,list,2);
-    List_Push_Back(int,list,3);
-    list->iterator = List_Begin(list);
-    while ( list->iterator != List_End(list))
-    {
-        printf("%d\n",*(int*)list->iterator->data);
-        list->iterator = list->iterator->next;
-    }
+    XD_INT          v1 = Int(4),//{{INT,4,Default_Compare,Default_Copy},11},
+                    v2 = Int(4);//{{INT,4,Default_Compare,Default_Copy},22};
+
+    XD_INT          test = Int(0);//{{INT,4,Default_Compare,Default_Copy},0};
+
+    XD_String       stest = String("");//{{STRING,5,Default_Compare,Default_Copy},""};
+    XD_Dict         *dict = calloc(1,sizeof(XD_Dict));
+
+    Dict_Init(dict,sizeof(XD_String),sizeof(XD_String));
+    Dict_Insert(dict,(&sk1),(&sv1));
+    printf("===========================\n");
+    Dict_Insert(dict,(&sk2),(&sv2));
+
+    Dict_Get(dict,(&sk1),(&stest));
+    printf("%s\n",stest._value);
 
 }
 #ifdef __cplusplus
