fb494291b97391ad0a27277408be245a074a04ce
ycsoft <xd.yang@yangmuyao.com>
2016-02-25 09:27:00 +0800

    library include file

--- ycdslib.h
+++ ycdslib.h
@@ -1,4 +1,17 @@
-#ifndef YCDSLIB_H
+﻿#ifndef YCDSLIB_H
 #define YCDSLIB_H
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include "cdef.h"
+#include "c_vector.h"
+#include "c_list.h"
+#include "c_rb_tree.h"
+#include "c_dict.h"
+
+#ifdef __cplusplus
+}
+#endif
 #endif // YCDSLIB_H
